07e96d318b0a7ff9cd97f994bf4b86179a9e581f
huzaifa0318 <121382281+huzaifa0318@users.noreply.github.com>
2023-05-20 05:00:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function setTextStyle(style) {
+    document.getElementById('text-area').style.fontStyle = style;
+}
+
+function setTextDecoration(decoration) {
+    document.getElementById('text-area').style.textDecoration = decoration;
+}
+
+function setFontWeight(fontWeight) {
+    document.getElementById('text-area').style.fontWeight = fontWeight;
+}
+
+function setTextColor(color) {
+    document.getElementById('text-area').style.color = color;
+}
